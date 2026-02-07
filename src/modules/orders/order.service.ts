65b3150b337141382c7b822d2bc33da7cfe0c926
Imtiajul islam <iimtiajul@gmail.com>
2026-02-07 13:25:02 +0600

    Added feature for OrderService Role Wise - Customer, Provider, Admin

--- src/modules/orders/order.service.ts
+++ src/modules/orders/order.service.ts
@@ -1,7 +1,8 @@
 import { Order } from "../../../prisma/generated/prisma/client";
 import { prisma } from "../../lib/prisma";
+import { UserRole } from "../../lib/type";
 
-const createOrder = async(data: Order, customerId: string) => {
+const createOrder = async (data: Order, customerId: string) => {
     // console.log(data, customerId);
     return await prisma.order.create({
         data: {
@@ -11,15 +12,57 @@ const createOrder = async(data: Order, customerId: string) => {
     })
 }
 
-const getOrder = async(customerId: string) => {
+const getOrder = async (customerId: string) => {
+    const getUserRole = await prisma.user.findUniqueOrThrow({
+        where: { id: customerId },
+        include: {
+            providerProfile:
+            {
+                select: {
+                    id: true,
+                    restaurantName: true
+                }
+            }
+        }
+    });
+    // admin get all Order info
+    if (getUserRole.role === UserRole.ADMIN) {
+        return await prisma.order.findMany();
+
+    }
+
+    //provider
+    if (getUserRole.role === UserRole.PROVIDER) {
+        // returning each provider profile
+       const promises = getUserRole.providerProfile.map(async (provider: { id: string, restaurantName: string }) => {
+            // console.log(provider.id)
+            const providerOrder = await prisma.order.findMany({
+                where: {
+                    providerId: provider.id
+                }
+            })
+            return {ProviderName: provider.restaurantName, ProviderId: provider.id, providerOrder};
+        })
+
+        return await Promise.all(promises);
+
+    }
     return await prisma.order.findMany({
         where: {
             customerId
         }
     })
+
+    //customer
+    return await prisma.order.findMany({
+        where: {
+            customerId
+        }
+    })
+
 }
 
-const getOrderById = async(id: string) => {
+const getOrderById = async (id: string) => {
     return await prisma.order.findMany({
         where: {
             id
